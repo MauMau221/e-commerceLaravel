e1106137c4ec9c0d18c96b85fb89d53471577da6
MauMau221 <mauriciobgsp@gmail.com>
2025-01-13 17:24:35 -0300

    Refazendo o layout

--- ecommerce/resources/views/home.blade.php
+++ ecommerce/resources/views/home.blade.php
@@ -1,23 +1,45 @@
 @extends('master')
 
-{{-- @include('nav.nav') --}}
-
 @section('content')
-    <nav>
-
+    <nav class="navbar navbar-expand-md navbar-dark bg-danger border-bottom shadow-sm mb-3">
+        <div class="container">
+            <a class="navbar-brand" href=""><strong>Testando Online</strong></a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target=".navbar-collapse"
+                aria-label="Abrir menu de navegação">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="navbar-collapse collapse">
+                <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Principal</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Politica de privacidade</a>
+                    </li>
+                </ul>
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Quero me cadastrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">Entrar</a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="#" class="nav-link text-white">
+                            <i class="fa-solid fa-cart-shopping"></i>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 
-
     <header>
-
     </header>
 
-    
     <main>
-
     </main>
 
     <footer>
-        
     </footer>
 @endsection
